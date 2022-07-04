2ca5c793f8fad5e4d8958bfc03191d34aaedaa38
Azatyk <azatuk2005@mail.ru>
2022-07-04 17:27:44 +0600

    fix: change code

--- saving-video.py
+++ saving-video.py
@@ -15,8 +15,7 @@ while cap.isOpened():
         # write the flipped frame
         out.write(frame)
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(0) & 0xFF == ord('q'):
             break
     else:
         break
