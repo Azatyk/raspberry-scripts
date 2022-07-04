6fec80b963bed4339f969e2051acf409ca3fea5d
Azatyk <azatuk2005@mail.ru>
2022-07-04 17:37:19 +0600

    fix: fix bug

--- saving-video.py
+++ saving-video.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 #fourcc = cv2.cv.CV_FOURCC(*'DIVX')
 #out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 out = cv2.VideoWriter(
-    '/home/pi/scripts/python-streaming-client/output.mp4', -1, 20.0, (640, 480))
+    '/home/pi/scripts/python-streaming-client/output.avi', -1, 20.0, (640, 480))
 
 while cap.isOpened():
     ret, frame = cap.read()
